33ab1cfa28d4dd60a53aab78ac68a73bf08dbe0b
Michael Duncan <mwduncan2018@gmail.com>
2023-12-25 22:36:50 -0500

    Updates

---
+++ 00B/04_GarbageGoTo.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <string.h>
+
+int main(void) {
+    char input[100];
+
+    while (1 == 1) {
+        printf("Enter a string: ");
+        scanf("%99s", input);
+        if (strcmp(input, "Garbage") == 0) {
+            goto UsingGoToIsGarbage;
+        }
+    }
+
+    UsingGoToIsGarbage:
+        printf("Why would anyone use GoTo? It is pure garbage.\n");
+
+    return 0;
+}
